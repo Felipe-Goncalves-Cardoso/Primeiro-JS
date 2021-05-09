c8a612dca03ff64b8eaa85323d3b4f02ab2e5acb
Felipe-Goncalves-Cardoso <80911918+Felipe-Goncalves-Cardoso@users.noreply.github.com>
2021-05-09 10:44:22 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,4 @@
+const anoAtual = 2021
+const nome = prompt("digite seu nome!") 
+const anoNascimento = parseInt(prompt("Digite seu ano de nascimento! "))
+alert(`Olá, seu nome é ${nome} e você tem ${anoAtual - anoNascimento}`)
